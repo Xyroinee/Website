c18fd389337da2b8cda5dba5bbb23d57107b8832
Xyroinee-ID <123985221+Xyroinee@users.noreply.github.com>
2023-06-13 07:05:42 +0700

    Add files via upload

---
+++ for-u/asset/script.js
@@ -0,0 +1,75 @@
+$('.tombol').click(function() {
+    // animate content
+    $('.halaman').addClass('animate_content');
+    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+
+    setTimeout(function() {
+        $('.halaman').removeClass('animate_content');
+    }, 3200);
+
+    //remove fadeIn class after 1500ms
+    setTimeout(function() {
+        $('.halaman').removeClass('fadeIn');
+    }, 1100);
+
+});
+
+
+$('.tombol2').click(function() {
+    // animate content
+    $('.halaman').addClass('animate_content');
+    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+
+    setTimeout(function() {
+        $('.halaman').removeClass('animate_content');
+    }, 3200);
+
+    //remove fadeIn class after 1500ms
+    setTimeout(function() {
+        $('.halaman').removeClass('fadeIn');
+    }, 1100);
+
+});
+
+
+$('.kehome').click(function() {
+    setTimeout(function() {
+        $('.home').addClass('fadeIn');
+    }, 1100);
+
+    PuterLagu();
+});
+
+
+$('.kehalaman2').click(function() {
+    setTimeout(function() {
+        $('.halaman2').addClass('fadeIn');
+    }, 1100);
+
+    JalaninTeks();
+    PuterLagu();
+    Ambilnama();
+});
+
+var i = 0;
+var txt = "Hidupku Indah Karena Kamu Bersamaku, Kamu Membuatku Bahagia Bahkan Jika Aku Merasa Sedih Dan Rendah. Senyummu Menerangi Hidupku Dan Semua Kegelapan Menghilang. Maukah Kamu Menjadi Milikku?\nJika Ingin Memberi Pesan Tap Salah Satu Di Bawah";
+var speed = 200;
+
+function JalaninTeks() {
+    if (i < txt.length) {
+        document.getElementById("tekss").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(JalaninTeks, speed);
+    }
+};
+
+
+function PuterLagu() {
+    var lagu = document.getElementById("lagu");
+    return lagu.paused ? lagu.play() : lagu.pause();
+};
+
+function Ambilnama() {
+    var x = document.getElementById("nama").value;
+    document.getElementById("tampilnama").innerHTML = x;
+};
